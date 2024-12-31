109ebb9b16cf24ab0d69c3aa4835aae86531f3e5
Dave Jammin Bacad <djbacad@gmail.com>
2024-12-31 16:43:48 +0800

    Refactor modifiers

--- src/modify_patch_notes.py
+++ src/modify_patch_notes.py
@@ -9,11 +9,28 @@ def add_default_hero_values(data):
     for hero in data.get("heroes", []):
         # Add default talent_notes if not present
         if "talent_notes" not in hero:
-            hero["talent_notes"] = ["No updates/changes"]
+            hero["talent_notes"] = ["No updates/changes in talents"]
+        else:
+            for talent in hero["talent_notes"]:
+                if "note" in talent:
+                    talent["note"] = "Changes in talent: " + talent["note"]
+
         if "hero_notes" not in hero:
-            hero["hero_notes"] = ["No updates/changes"]
+            hero["hero_notes"] = ["No updates/changes in base stats"]
+        else:
+            for hero_note in hero["hero_notes"]:
+                if "note" in hero_note:
+                    hero_note["note"] = "Changes in hero's base or general stats: " + hero_note["note"]
+
         if "abilities" not in hero:
             hero["abilities"] = []
+        else:
+            for ability in hero.get("abilities", []):
+                for ability_note in ability.get("ability_notes", []):
+                    if "note" in ability_note:
+                        ability_note["note"] = "Changes in ability: " + ability_note["note"]
+
+
 
         # Handle subsections for facets
         if "subsections" in hero:
